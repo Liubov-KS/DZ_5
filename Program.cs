44095bd2ece25638bef0642b3f07e9514921b26b
Liubov-Knyazevskaya <liubanya09@gmail.com>
2022-11-06 23:04:59 +0300

    Task 38 Random Difference between Min Max

--- Program.cs
+++ Program.cs
@@ -52,7 +52,8 @@ Console.WriteLine($"Количество чётных чисел в массив
                                 sum = num + sum  */
 // [3, 7, 23, 12] -> 19
 //  [-4, -6, 89, 6] -> 0
-Console.Write($"Введите количество элементов массива: ");
+
+/* Console.Write($"Введите количество элементов массива: ");
 int numberElements = Convert.ToInt32(Console.ReadLine()); 
 
 int RandomNumbers(int numberElements, int min, int max)
@@ -78,8 +79,35 @@ int randomNumbers =  RandomNumbers(numberElements, 1, 10);
 
 Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях: {randomNumbers}");
 
+*/
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // Задача 38: Задайте массив вещественных чисел. 
 // Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
+
+double[] arrayRealNumbers = new double[10];
+  for (int i = 0; i < arrayRealNumbers.Length; i++ )
+  {
+    arrayRealNumbers[i] = new Random().Next(1, 10);
+    Console.Write(arrayRealNumbers[i] + " ");
+  }
+
+double maxNumber = arrayRealNumbers[0];
+double minNumber = arrayRealNumbers[0];
+
+  for (int i = 1; i < arrayRealNumbers.Length; i++)
+  {
+    if (maxNumber < arrayRealNumbers[i])
+    {
+      maxNumber = arrayRealNumbers[i];
+    }
+        if (minNumber > arrayRealNumbers[i])
+    {
+      minNumber = arrayRealNumbers[i];
+    }
+  }
+
+  double decision = maxNumber - minNumber;
+
+  Console.WriteLine($"\nразница между между максимальным ({maxNumber}) и минимальным({minNumber}) элементами: {decision}");
